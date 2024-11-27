8b1c86d7e4dbceb233b97dc1200c84a180c12544
est_calexx <115084853+carljohannesmunoz@users.noreply.github.com>
2024-11-27 08:16:41 +0000

    Add files via upload

---
+++ ProgrammingExercise6/Group10_PE6_problem1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int isPrime(int number) {
+    int factors = 0;
+    if(number > 1) {
+        for(int checker = 1; checker<=number; checker++) {
+            if(checker<=number && number%checker==0) 
+                factors++;
+        }
+        if(factors==2) return 1;
+        else return 0;
+    }
+    return 0;
+}
+
+int isPerfect(int number) {
+    int divisors = 0;
+    for(int  j=1; j<=number; j++) {
+        if(number%j==0) divisors+=j;
+        if(divisors==number) return 1;
+    }
+    return 0;
+}
+
+int main() {
+    int number;
+    printf("Enter a positive integer(Ctrl-Z to end): ");
+    while(scanf("%d", &number) != EOF) {
+        (isPrime(number)==0) ? printf(" prime: NO\n") : printf(" prime: YES\n");
+        (isPerfect(number)==0) ? printf(" perfect: NO\n\n") : printf(" perfect: YES\n\n");
+        printf("Enter a positive integer(Ctrl-Z to end): ");
+    }
+    printf("\n End of program. Thanks!\n");
+
+    return 0;
+}
---
+++ ProgrammingExercise6/Group10_PE6_problem2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int occurenceCounter(int number, int digit) {
+    if(number<0) number*=-1;
+    if(number!=0) {
+        int lastDigit = number%10;
+        int digitCount = (digit==lastDigit) ? 1 : 0;
+        return digitCount + occurenceCounter(number/10, digit);
+    } else {
+        return 0;
+    }
+}
+
+
+
+int main() {
+    int number;
+    FILE *ifp;
+
+    ifp = fopen("Integers.txt", "rt");
+    printf("INTEGER \t0s \t1s \t2s \t3s \t4s \t5s \t6s \t7s \t8s \t9s\n");
+    printf("-------------------------------------------------------------------------------------------\n");
+    while(1) {
+        fscanf(ifp, "%d", &number);
+        if(number==0) break;
+        printf("%d\t\t", number);
+        for(int digit=0; digit<=9; digit++) {
+            int digitCount = occurenceCounter(number, digit);
+            printf("%d\t ", digitCount);
+        }
+        printf("\n");
+    }
+    fclose(ifp);
+    return 0;
+}
